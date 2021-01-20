1483981522574050d719b610c0d0967b0a286f96
Chary Vaangeepuram <srivathsalachary@gmail.com>
2021-01-20 11:58:25 -0800

    Update setup-proxy.sh

--- ocean/setup-proxy.sh
+++ ocean/setup-proxy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-DOMAIN="www.gamyam.net"
-JENKINS_IPV4=
+DOMAIN="malathi.gamyam.net"
+JENKINS_IPV4=206.189.74.225
 JENKINS_HTTP_PORT=8080
-JENKINS_IPV6=
+JENKINS_IPV6=2604:a880:2:d0::2075:e001
 JENKINS_HTTPS_PORT=8081
 
-NGINX_IPV4=
+NGINX_IPV4=206.189.74.225
 NGINX_HTTP_PORT=80
-NGINX_IPV6=
+NGINX_IPV6=2604:a880:2:d0::2075:e001
 NGINX_HTTPS_PORT=443
 
 CUR_USER=`whoami`
@@ -15,20 +15,6 @@ if [ "$CUR_USER" != "root" ]; then
   echo "Not root yet"
   exit 1
 fi
-
-if [ "" == "$1" ]; then
-  echo "Please provide domain name:"
-  echo "  setup-proxy.sh full.domain.name Private.IP.Address"
-  exit 1
-fi
-DOMAIN=$1
-
-if [ "" == "$2" ]; then
-  echo "Please provide Private IP address to Jenkins master"
-  echo "  setup-proxy.sh full.domain.name Private.IP.Address"
-  exit 1
-fi
-IPADDR=$2
 
 cd /etc/nginx/sites-available
 if [ -f webproxy.conf ]; then
